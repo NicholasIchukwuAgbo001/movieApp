3848c5699595fad1bc36a5ee206d8ed6e40e6b84
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-16 15:27:17 +0100

    commit

--- src/api/MovieApi.jsx
+++ src/api/MovieApi.jsx
@@ -1,5 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const movieBaseUrl = "https://api.themoviedb.org/3/movie"
+
 export const MovieApiSlice = createApi({
     reducerPath: "movieApi",
     baseQuery: fetchBaseQuery({
