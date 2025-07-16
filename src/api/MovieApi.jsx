26fd88521701daf918e3a7bbf9d1624ddc54c2cd
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-16 15:52:07 +0100

    commit

--- src/api/MovieApi.jsx
+++ src/api/MovieApi.jsx
@@ -1,13 +1,14 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const movieBaseUrl = "https://api.themoviedb.org/3/movie"
+const movieBaseUrl = "https://api.themoviedb.org/3/movie";
+const API_KEY = import.meta.env.VITE_API_MOVIE_API_KEY;
 
 export const MovieApiSlice = createApi({
-    reducerPath: "movieApi",
-    baseQuery: fetchBaseQuery({ baseUrl:``}),
-    endpoints: (builder) => ({
-        getPopularMovies: builder.query({
-            query:()=>(`/popular`)
-        })
-    })
-})
+  reducerPath: "movieApi",
+  baseQuery: fetchBaseQuery({ baseUrl: movieBaseUrl }),
+  endpoints: (builder) => ({
+    getPopularMovies: builder.query({
+      query: () => `/popular?api_key=${API_KEY}`,
+    }),
+  }),
+});
