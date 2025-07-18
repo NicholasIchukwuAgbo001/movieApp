5d3ed4cf308d970ac822d8418cf0ae4cfea139db
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-18 11:47:27 +0100

    commit

--- src/reuseable/MovieCard.jsx
+++ src/reuseable/MovieCard.jsx
@@ -3,11 +3,12 @@ import { useGetPopularMoviesQuery } from "../api/MovieApi";
 const MovieCard = () => {
 
   const {data} = useGetPopularMoviesQuery();
+  const IMG_URL = "https://image.tmdb.org/t/p/w500"
 
   return (
     <div>
       {data?.results.map((result) => (
-        <div>
+        <div key={result.id}>
           <p> {result.title} </p>
           <p> {result.overview} </p>
           <p> {result.vote_average} </p>
