b4a6a388d67b7d635973722b7fca06e7cf15a5bc
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-18 12:02:24 +0100

    commit

--- src/reuseable/MovieCard.jsx
+++ src/reuseable/MovieCard.jsx
@@ -5,13 +5,12 @@ const MovieCard = () => {
   const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
   return (
-    <div>
+    <div  className="grid grid-cols-4 gap-8">
       {data?.results.map((result) => (
         <div key={result.id}>
-          <img src={`${IMG_URL}${result.poster_path}`} alt="img" />
+          <img src={`${IMG_URL}${result.poster_path}`} alt="img" className="w-50" />
           <p>{result.title}</p>
-          <p>{result.overview}</p>
-          <p>{result.vote_average}</p>
+          <p>⭐⭐⭐{result.vote_average}</p>
         </div>
       ))}
     </div>
